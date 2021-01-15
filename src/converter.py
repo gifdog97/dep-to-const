22bfc6bb5d5ca504c766cb81d9f3a2858c501340
gifdog97 <omusubset.s@gmail.com>
2021-01-15 13:43:18 +0900

    fix dataset for syntactic evaluation

--- src/converter.py
+++ src/converter.py
@@ -14,14 +14,12 @@ logger.propagate = False
 
 source_path_mapping = {
     'English_EWT': 'UD_English-EWT/en_ewt-ud-{}.conllu',
-    'English_GUM': 'UD_English-GUM/en_gum-ud-{}.conllu',
-    'Japanese': 'UD_Japanese-GSD/ja_gsd-ud-{}.conllu',
-    'Arabic': 'UD_Arabic-PADT/ar_padt-ud-{}.conllu',
-    'Chinese': 'UD_Chinese-GSD/zh_gsd-ud-{}.conllu',
-    'Czech': 'UD_Czech-PDT/cs_pdt-ud-{}.conllu',
-    'Russian': 'UD_Russian-GSD/ru_gsd-ud-{}.conllu',
-    'Spanish': 'UD_Spanish-GSD/es_gsd-ud-{}.conllu',
-    'German_HDT': 'UD_German-HDT/de_hdt-ud-{}.conllu'
+    'Japanese_GSD': 'UD_Japanese-GSD/ja_gsd-ud-{}.conllu',
+    'French_GSD': 'UD_French-GSD/fr_gsd-ud-{}.conllu',
+    'German_GSD': 'UD_German-GSD/de_gsd-ud-{}.conllu',
+    'Hebrew_HTB': 'UD_Hebrew-HTB/he_htb-ud-{}.conllu',
+    'Russian_GSD': 'UD_Russian-GSD/ru_gsd-ud-{}.conllu',
+    'ptb-to-ud': 'ptb-to-ud/{}.conllu'
 }
 
 parser = argparse.ArgumentParser()
